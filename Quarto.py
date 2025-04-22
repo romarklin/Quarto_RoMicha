1280143220d5b11d569b95a21b1da233506cf11c
romarklin <halewyckromain@gmail.com>
2025-04-22 09:06:35 +0200

    CACA

--- Quarto.py
+++ Quarto.py
@@ -1,1 +1,15 @@
 import math
+
+
+
+
+class Game:
+    def __init__(self, B, S, D, L, E, F, C, P):
+        self.B = B #"Big"
+        self.S = S #"Small"
+        self.D = D #"Dark"
+        self.L = L #"Light"
+        self.E = E #"Empty"
+        self.F = F #"Full"
+        self.C = C #"Cylinder"
+        self.P = P #"Prism"
