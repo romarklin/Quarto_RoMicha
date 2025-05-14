ed39cf750abbb5a3cb2294b9681c486895a0a36a
DocteurGaufre <michaelspackitch@gmail.com>
2025-05-14 12:32:48 +0200

    Nettoyage du code

--- Quarto.py
+++ Quarto.py
@@ -1,18 +1,14 @@
 import random as r
 import socket
 import json
-import os
 
 class Game:
     def __init__(self):
         s = socket.socket()
-        s.bind(("0.0.0.0", 8888))
+        s.bind(("0.0.0.0", 8885))
         s.listen()
 
-        """with open('test.json','r') as file:
-            test = json.load(file)"""
-
-        self.IndicesGagnants = [[0,1,2,3],[4,5,6,7],[8,9,10,11],[12,13,14,15],      #Donne les indices des cases gagnantes
+        self.IndicesGagnants = [[0,1,2,3],[4,5,6,7],[8,9,10,11],[12,13,14,15], #Donne les indices des cases gagnantes
                    [0,4,8,12],[1,5,9,13],[2,6,10,14],[3,7,11,15],
                    [0,5,10,15],[3,6,9,12]
                     ]
@@ -29,8 +25,8 @@ class Game:
         pong_data = json.dumps(pong)
         self.jeu_data = json.dumps(self.jeu)
 
-        """while True:
-            client, addr = s.accept()           # Accepter la connexion
+        while True:
+            client, addr = s.accept()       # Accepter la connexion
             while True:                     # Boucle de jeu pour le serveur
                 reponse = client.recv(4096).decode()
                 if not reponse:
@@ -45,6 +41,7 @@ class Game:
                     etat_du_jeu = message.get("state")
                     self.plateau = etat_du_jeu['board']
                     self.piece_a_jouer = etat_du_jeu['piece']
+                    print(f"Pièce reçue : {self.piece_a_jouer}")
                     print(message.get("errors"))
 
                     self.vraie_position = None
@@ -55,15 +52,13 @@ class Game:
                     colis = {
                         "response" : "move",
                         "move" : self.jeu,
-                        "message" : "ok"
+                        "message" : "oui"
                     }
-
-                    #print(f"colis = {colis}")
 
                     colis_data = json.dumps(colis)
                     client.sendall(colis_data.encode('utf-8'))
 
-            client.close()"""
+            client.close()
 
     def give_piece(self):   #Donne la pièce avec caractéristiques les moins présentes sur le plateau
         carac = {"B":0,"S":0,"L":0,"D":0,"F":0,"E":0,"P":0,"C":0}
@@ -87,7 +82,7 @@ class Game:
                     Pions_dict[pion] += 1
 
         maximum_pion = max(Pions_dict, key=Pions_dict.get)
-        #print(f"pion stratégique classique : {maximum_pion}")
+        print(f"Piece à donner : {maximum_pion}")
         self.jeu["piece"] = maximum_pion
 
     def give_piece_urgence(self, indices): #Si 3 alignées alors urgence de ne pas donner la mauvaise pièce
@@ -117,29 +112,28 @@ class Game:
                     for pion in self.Pions:
                         if antidote in pion:
                             pion_a_donner = pion
-                            print(f"antidote = {pion_a_donner}")
                             self.vrai_pion = True
                             self.jeu["piece"] = pion_a_donner
+                            print(f"Pièce à donner urgente : {pion_a_donner}")
                             break
                     break  
 
     def place(self, position): #Donner la position sur laquelle on place le pion
         if position != None:
-            print(f"position stratégique = {position}")
+            pass
         else:
             Pos_possibles = []
             for numero in range(len(self.plateau)):
                 if self.plateau[numero] == None:
                     Pos_possibles.append(numero)
             position = r.choice(Pos_possibles)
-            print(f"position random = {position}")
+
+        print(f"Position choisie : {position}")
 
         self.jeu["pos"] = position
     
     def move(self, indices):
         pieces_placees = []
-        """global vraie_position
-        vraie_position = None"""
         for emplacement in indices:
             pieces_placees.append(self.plateau[emplacement]) #Prend les emplacements rangée par rangée
 
@@ -183,8 +177,6 @@ class Game:
             "BDFC"
             }
 
-        print(self.Pions)
-
         for piece in self.plateau: #On enlève les pièces déjà placées
             if piece != None:
                 pion_trouve = next((p for p in self.Pions if sorted(p) == sorted(piece)), None)
@@ -196,15 +188,14 @@ class Game:
             if pion_trouve:
                 self.Pions.remove(pion_trouve)
 
-    
-        for liste_indice in self.IndicesGagnants:  #On parcourt les indices gagnants
+        for liste_indice in self.IndicesGagnants: #On parcourt les indices gagnants
             if self.vraie_position == None and self.piece_a_jouer != None:
                 self.move(liste_indice)
             
         if self.vraie_position == None:  
             self.place(None)
 
-        if self.vraie_position == None:     #Donne pièce de "contrage"
+        if self.vraie_position == None: #Donne pièce de "contrage"
             for liste_indice in self.IndicesGagnants:
                 self.give_piece_urgence(liste_indice)
                 if self.vrai_pion == True:
@@ -212,6 +203,5 @@ class Game:
 
         if self.vrai_pion == False:
             self.give_piece()
-
 
 Game()
--- inscription_RoMicha.py
+++ inscription_RoMicha.py
@@ -2,13 +2,13 @@ import socket
 import json
 
 s = socket.socket()
-s.connect(("172.17.89.80", 3000))
+s.connect(("localhost", 3000))
 
 requete = {
   "request": "subscribe",
   "port": 8885,
-  "name": "RoMicha",
-  "matricules": ["23093", "22134"]
+  "name": "MDR",
+  "matricules": ["23023", "22134"]
 }
 
 requete_data = json.dumps(requete)
